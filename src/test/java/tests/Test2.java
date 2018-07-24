f0334f8bde5bf14a419a4e0aac7105d3146c8891
Ciuma <mihaela.ciuma.ext@siemens.com>
2018-07-24 14:39:32 +0200

    add test4

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -1,5 +1,30 @@
 package tests;
 
-public class Test2 {
+import org.testng.annotations.BeforeMethod;
+
+import testautomation.Cart;
+import testautomation.CheckKey;
+import testautomation.DetailsStiletto;
+import testautomation.HomePageStiletto;
+import testautomation.KeyWord;
+import testautomation.TestBase;
+
+
+public class Test2 extends TestBase {
+	HomePageStiletto homePageStiletto;
+	DetailsStiletto  detailsStiletto;
+	Cart cart;
+	KeyWord key;
+	CheckKey check;
+	
+	@BeforeMethod
+	public void beforeMTD() {
+		homePageStiletto = new HomePageStiletto(driver);
+		detailsStiletto  = new DetailsStiletto(driver);
+		cart = new Cart(driver);
+		key = new KeyWord(driver);
+		check= new CheckKey(driver);
+		}
+
 
 }
